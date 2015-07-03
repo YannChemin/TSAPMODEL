470a30c5c6d2bffb1861c99d2af8ca5eb06055db
Yann Chemin <ychemin@gmail.com>
2015-07-03 23:19:48 +0530

    First draft masking arguments

--- irrigation/run.sh
+++ irrigation/run.sh
@@ -12,4 +12,9 @@ echo "TL.py 0 1 0 0 1 1.0,1.0,1.0,1.0,1.0 m,l,l,m,m"
 
 python -i TL.py 0 1 0 0 1 1.0,1.0,1.0,1.0,1.0 m,l,l,m,m
 
+echo "Second test: proxroad + wet + proxriv + dwell + irrhead + weigt=1.0,1.0,1.0,1.0,1.0 + better=m,l,l,m,m + scren=TOPOZONE=2"
+echo "TL.py 0 1 0 0 1 1.0,1.0,1.0,1.0,1.0 m,l,l,m,m TOPOZONE,==,2"
+
+python -i TL.py 0 1 0 0 1 1.0,1.0,1.0,1.0,1.0 m,l,l,m,m TOPOZONE,==,2
+
 
